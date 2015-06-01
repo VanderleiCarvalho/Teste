246d1fce471d01027c8d00b57f6cf19715cec2fd
VanderleiCarvalho <vclecheniooski@gmail.com>
2015-06-01 19:25:28 -0300

    3 modificação

---
+++ teste3.java
@@ -0,0 +1,21 @@
+17 sloc)  0.537 kb
+importar  javax.swing. * ;
+
+/ *
+ * Para alterar este cabeçalho de licença, escolha cabeçalhos de Licenciamento em Propriedades do projeto.
+ * Para alterar esse arquivo de modelo, escolha Ferramentas | Modelos
+ * E abra o modelo no editor.
+ * /
+
+/ **
+ *
+ *author Carvalho
+ * /
+public  class  Teste {
+    public  static void  Main ( de corda [] args ) {
+        Mensagem msg0 =  nova  Mensagem ();
+        msg0 . setTexto ("Modificação do Projeto 3" );
+        JOptionPane . showMessageDialog ( nulo , msg0 . getTexto ());
+    }
+    
+}
