6124038642ed0a5efaefebb584d1e687693c5ccf
VanderleiCarvalho <vclecheniooski@gmail.com>
2015-06-01 19:26:19 -0300

    Create mensagem3.java

---
+++ mensagem3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author carvalho
+ */
+public class Mensagem3 {
+    private String texto;
+
+    /**
+     * @return the texto
+     */
+    public String getTexto() {
+        return texto;    }
+
+    /**
+     * @param texto the texto to set
+     */
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+    
+}
